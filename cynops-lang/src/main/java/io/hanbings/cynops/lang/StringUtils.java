c2469bbdf058964baabbb735e6e8f583f4c220bf
Hanbings <3219065882@qq.com>
2021-08-18 22:00:49 +0800

    🚧 Feat: String utils working...

--- cynops-lang/src/main/java/io/hanbings/cynops/lang/StringUtils.java
+++ cynops-lang/src/main/java/io/hanbings/cynops/lang/StringUtils.java
@@ -23,8 +23,8 @@ public class StringUtils {
      * 传说中有一种ascii移位转换首字母的 <br>
      * 但似乎需要几层判断 有需要以后再补
      *
-     * @param source 输入字符串
-     * @return 返回字符串
+     * @param source 字符串
+     * @return 返回转换后的字符串
      */
     public static String initialToUpperCase(String source) {
         return source.substring(0, 1).toUpperCase() + source.substring(1);
@@ -32,10 +32,41 @@ public class StringUtils {
 
     /**
      * 字符串首字母小写
-     * @param source 输入字符串
-     * @return 返回字符串
+     *
+     * @param source 字符串
+     * @return 返回转换后的字符串
      */
-    public static String initialToLowerCase(String source){
-        return source.substring(0,1).toLowerCase() + source.substring(1);
+    public static String initialToLowerCase(String source) {
+        return source.substring(0, 1).toLowerCase() + source.substring(1);
+    }
+
+    /**
+     * 判断首字母是否为大写
+     *
+     * @param source 字符串
+     * @return 返回是否首字母为大写
+     */
+    public static boolean isInitialUpperCase(String source) {
+        return source.toCharArray()[0] >= 65 && source.toCharArray()[0] <= 90;
+    }
+
+    /**
+     * 判断字符串是否为小写
+     *
+     * @param source 字符串
+     * @return 返回是否首字母为小写
+     */
+    public static boolean isInitialLowerCase(String source) {
+        return source.toCharArray()[0] >= 97 && source.toCharArray()[0] <= 122;
+    }
+
+    /**
+     * 判断字符串长度是否为0
+     *
+     * @param source 字符串
+     * @return "" 是 false " " 或其他形式存在字符 是 ture
+     */
+    public static boolean hasLength(String source) {
+        return (source != null && source.length() > 0);
     }
 }
