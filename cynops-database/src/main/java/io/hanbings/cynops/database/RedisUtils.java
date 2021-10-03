6910f07e10ef825b9967709ad8ff810c6d21a9a9
hanbings <3219065882@qq.com>
2021-10-03 16:26:08 +0800

    ✨ Feat: Mongodb Redis and SQlite connecter.

--- cynops-database/src/main/java/io/hanbings/cynops/database/RedisUtils.java
+++ cynops-database/src/main/java/io/hanbings/cynops/database/RedisUtils.java
@@ -1,5 +1,46 @@
 package io.hanbings.cynops.database;
 
+import redis.clients.jedis.Jedis;
+
 @SuppressWarnings("unused")
 public class RedisUtils {
+    /**
+     * 使用默认端口 链接 无认证 redis
+     *
+     * @param address 地址
+     * @return Redis 连接
+     */
+    @Deprecated
+    public static Jedis getRedisConnection(String address) {
+        return new Jedis(address);
+    }
+
+    /**
+     * 获取 Redis 连接 无用户名认证
+     *
+     * @param address  地址
+     * @param port     端口
+     * @param password 密码
+     * @return Redis 连接
+     */
+    public static Jedis getRedisConnection(String address, int port, String password) {
+        Jedis jedis = new Jedis(address, port);
+        jedis.auth(password);
+        return jedis;
+    }
+
+    /**
+     * 获取 Redis 连接
+     *
+     * @param address  地址
+     * @param port     端口
+     * @param username 用户名
+     * @param password 密码
+     * @return Redis 连接
+     */
+    public static Jedis getRedisConnection(String address, int port, String username, String password) {
+        Jedis jedis = new Jedis(address, port);
+        jedis.auth(username, password);
+        return jedis;
+    }
 }
--- cynops-database/src/main/java/io/hanbings/cynops/database/SQLUtils.java
+++ cynops-database/src/main/java/io/hanbings/cynops/database/SQLUtils.java
@@ -5,18 +5,33 @@ import java.util.regex.Pattern;
 
 @SuppressWarnings("unused")
 public class SQLUtils {
+    /**
+     * 使用工具包自带的规则
+     *
+     * @param sql sql 语句
+     * @return 对于有注入风险的语句返回 true
+     */
     public static boolean checkInject(String sql) {
         Pattern pattern = Pattern.compile(SQLUtils.defaultRegex);
         Matcher matcher = pattern.matcher(sql.toLowerCase());
         return matcher.find();
     }
 
+    /**
+     * 接受自定义正则的检查器
+     *
+     * @param sql   sql 语句
+     * @param regex 正则表达式
+     * @return 返回判断结果
+     */
     public static boolean checkInject(String sql, String regex) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(sql.toLowerCase());
         return matcher.find();
     }
 
+    // 屏蔽没别的 看得不太爽而已
+    @SuppressWarnings("ALL")
     public static String defaultRegex = "\\b(and|exec|insert|select|drop|grant|alter|delete" +
             "|update|count|chr|mid|master|truncate|char|declare|or)\\b|(\\*|;|\\+|'|%)\n";
 }
