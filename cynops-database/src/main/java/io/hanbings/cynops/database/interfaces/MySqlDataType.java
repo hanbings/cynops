504e56efeb305c99325d2a56baa9b8685d3ee9a4
hanbings <3219065882@qq.com>
2021-11-07 10:26:34 +0800

    ✨ Feat: MySql CURD sql builder.

---
+++ cynops-database/src/main/java/io/hanbings/cynops/database/interfaces/MySqlDataType.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021 Hanbings / hanbings Cynops Toolbox.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *           http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.hanbings.cynops.database.interfaces;
+
+@SuppressWarnings("unused")
+public class MySqlDataType {
+    public static final String TINYINT = "TINYINT";
+    public static final String SMALLINT = "SMALLINT";
+    public static final String MEDIUMINT = "MEDIUMINT";
+    public static final String INT = "INT";
+    public static final String INTEGER = "INTEGER";
+    public static final String BIGINT = "BIGINT";
+    public static final String FLOAT = "FLOAT";
+    public static final String DOUBLE = "DOUBLE";
+    public static final String DEC = "DEC";
+    public static final String DECIMAL = "DECIMAL";
+
+    public static final String DATE = "DATE";
+    public static final String TIME = "TIME";
+    public static final String YEAR = "YEAR";
+    public static final String DATETIME = "DATETIME";
+    public static final String TIMESTAMP = "TIMESTAMP";
+
+    public static final String CHAR = "CHAR";
+    public static final String VARCHAR = "VARCHAR";
+    public static final String TINYBLOB = "TINYBLOB";
+    public static final String BLOB = "BLOB";
+    public static final String TEXT = "TEXT";
+    public static final String MEDIUMBLOB = "MEDIUMBLOB";
+    public static final String MEDIUMTEXT = "MEDIUMTEXT";
+    public static final String LONGBLOB = "LONGBLOB";
+    public static final String LONGTEXT = "LONGTEXT";
+}
